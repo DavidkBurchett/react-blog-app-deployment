63179142945fe947e5982e35460c810472b0ba12
DavidkBurchett <67274544+DavidkBurchett@users.noreply.github.com>
2025-11-10 16:18:29 -0500

    Update Homepage.jsx
    Fixed Footer call

--- src/components/Homepage/Homepage.jsx
+++ src/components/Homepage/Homepage.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 import { Header } from "../common/Header";
-import { Footer } from "../common/footer";
+import { Footer } from "../common/Footer";
 
 export function Homepage() {
     return (
